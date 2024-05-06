c2606c04159cc06a19c1e06b9bac5e60d55f5d48
soohee Jang <shjang0820@hansung.ac.kr>
2024-05-06 22:39:40 +0900

    feature done

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,14 +1,20 @@
 import React from "react";
 import '../css/graph.css';
-const Statistics = ()=> {
-
+const Statistics = ({ weeklySales })=> {
+    const changeRate = ((weeklySales.thisWeekTotalSales - weeklySales.lastWeekTotalSales) / weeklySales.lastWeekTotalSales * 100).toFixed(2);
+    
+    // 아이콘 및 색상 결정 로직
+    const iconStyle = changeRate >= 0 ? { symbol: '🔺', color: 'red' } : { symbol: '🔻', color: 'blue' };
+//▼
     return(
         <div className="container">
             <div className="componentTitle">판매 통계</div>
             <div className="statistics">
-                <div className="sales">저번 주 매출 금액 &nbsp;&nbsp;&nbsp; 200,000</div>
-                <div className="sales">이번 주 매출 금액 &nbsp;&nbsp;&nbsp; 230,000</div>
-                <div className="profit"> 지난 주 대비 매출 증감율   &nbsp;&nbsp;&nbsp; 🔺 15%</div>
+                <div className="sales">저번 주 매출 금액 &nbsp;&nbsp;&nbsp; {weeklySales.lastWeekTotalSales}</div>
+                <div className="sales">이번 주 매출 금액 &nbsp;&nbsp;&nbsp; {weeklySales.thisWeekTotalSales}</div>
+                <div className="profit"> 지난 주 대비 매출 증감율   &nbsp;&nbsp;&nbsp;
+                    <span style={{ color: iconStyle.color }}>{iconStyle.symbol}</span> {changeRate}% 
+                </div>
             </div>
         </div>
     );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Graph from '../components/Graph';
 import Statistics from '../components/Statistics';
 import '../css/main.css';
 
 function Main(){
+    const [weeklySales, setWeeklySales] = useState(null);
+
+    useEffect(() => {
+        // API 호출
+        axios.get('http://localhost:8080/seller/sales')
+            .then(response => {
+                // API로부터 데이터 가져오기 성공 시
+                setWeeklySales(response.data);
+            })
+            .catch(error => {
+                // API로부터 데이터 가져오기 실패 시
+                console.error('Error fetching weekly sales data:', error);
+            });
+    }, []);
     return(
         <div>
             <h1 className='title'>빵 판매 현황</h1>
             <div className='graphContainer'>
-                <Statistics/>
-               <Graph/>
-
+            {weeklySales && (
+                    <>
+                <Statistics weeklySales={weeklySales} />
+                <Graph weeklySales={weeklySales} />
+                </>
+            )}
             </div>
-        </div>
-
-        
-    )
-    
+        </div> 
+    ); 
 }
 
 export default Main;
